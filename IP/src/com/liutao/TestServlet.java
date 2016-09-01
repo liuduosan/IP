d6c8133cd486d251df79bcadbb71b88bede964b1
liutaoq <liuduosan@163.com>
2016-09-01 21:35:03 +0800

    首次导入。

---
+++ IP/src/com/liutao/TestServlet.java
@@ -0,0 +1,154 @@
+package com.liutao;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("=========get===========");
+		
+		//方式一
+		String ip =  request.getRemoteAddr(); 
+		System.out.println("ip="+ip);
+		String queryIPUrl = "http://ip.taobao.com/service/getIpInfo.php";
+		//方式二
+//		String ip = getIpAddr(request);
+		String result = sendGet(queryIPUrl,"ip="+ip);
+		System.out.println("result="+result);
+		PrintWriter out = response.getWriter();
+		out.print(result);
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("=========post===========");
+	}
+	
+	/**
+	 * =============例子=================
+	 * WL-Proxy-Client-IP=211.161.1.239  
+	 * Proxy-Client-IP=211.161.1.239  
+	 * X-Forwarded-For=211.161.1.239  
+	 * WL-Proxy-Client-Keysize=   
+	 * WL-Proxy-Client-Secretkeysize=   
+	 * X-WebLogic-Request-ClusterInfo=true  
+	 * X-WebLogic-KeepAliveSecs=30  
+	 * X-WebLogic-Force-JVMID=-327089098  
+	 * WL-Proxy-SSL=false  
+	 * @param request
+	 * @return
+	 */
+	private String getIpAddr(HttpServletRequest request) {   
+	     String ipAddress = null;   
+	     //ipAddress = request.getRemoteAddr();   
+	     ipAddress = request.getHeader("x-forwarded-for");   
+	     if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {   
+	      ipAddress = request.getHeader("Proxy-Client-IP");   
+	     }   
+	     if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {   
+	         ipAddress = request.getHeader("WL-Proxy-Client-IP");   
+	     }   
+	     if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {   
+	      ipAddress = request.getRemoteAddr();   
+	      if(ipAddress.equals("127.0.0.1")){   
+	       //根据网卡取本机配置的IP   
+	       InetAddress inet=null;   
+	    try {   
+	     inet = InetAddress.getLocalHost();   
+	    } catch (UnknownHostException e) {   
+	     e.printStackTrace();   
+	    }   
+	    ipAddress= inet.getHostAddress();   
+	      }   
+	            
+	     }   
+	  
+	     //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割   
+	     if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15   
+	         if(ipAddress.indexOf(",")>0){   
+	             ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));   
+	         }   
+	     }   
+	     return ipAddress;    
+	  }   
+	
+	 private String sendGet(String url, String param) {
+	        String result = "";
+	        BufferedReader in = null;
+	        try {
+	            String urlNameString = url + "?" + param;
+	            URL realUrl = new URL(urlNameString);
+	            // 打开和URL之间的连接
+	            URLConnection connection = realUrl.openConnection();
+	            // 设置通用的请求属性
+	            connection.setRequestProperty("accept", "*/*");
+	            connection.setRequestProperty("connection", "Keep-Alive");
+	            connection.setRequestProperty("user-agent",
+	                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+	            // 建立实际的连接
+	            connection.connect();
+	            // 获取所有响应头字段
+	            Map<String, List<String>> map = connection.getHeaderFields();
+	            // 遍历所有的响应头字段
+	            for (String key : map.keySet()) {
+	                System.out.println(key + "--->" + map.get(key));
+	            }
+	            // 定义 BufferedReader输入流来读取URL的响应
+	            in = new BufferedReader(new InputStreamReader(
+	                    connection.getInputStream()));
+	            String line;
+	            while ((line = in.readLine()) != null) {
+	                result += line;
+	            }
+	        } catch (Exception e) {
+	            System.out.println("发送GET请求出现异常！" + e);
+	            e.printStackTrace();
+	        }
+	        // 使用finally块来关闭输入流
+	        finally {
+	            try {
+	                if (in != null) {
+	                    in.close();
+	                }
+	            } catch (Exception e2) {
+	                e2.printStackTrace();
+	            }
+	        }
+	        return result;
+	    }
+
+}
